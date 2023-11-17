25f22e091f41a93fdef256e1b50be7abbc547466
Caleb Hargraves <jmjoyh00@gmail.com>
2023-11-16 19:20:27 -0600

    Updating files for Point 3.

---
+++ Part 2/Point 3 - Combining Conditions/age_check.py
@@ -0,0 +1,7 @@
+age = int(input("What is your age?"))
+if age < 0:
+    print("That must be a mistake")
+elif age < 5:
+    print("I suspect you can't write quite yet...")
+else:
+    print(f"Ok, you're {age} years old")
