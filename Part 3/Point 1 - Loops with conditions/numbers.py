84a1991ca4c94f6a2b37eea28c26d38194a70fc0
Caleb Hargraves <jmjoyh00@gmail.com>
2023-11-28 19:42:56 -0600

    Did 1 Exercise from Part 3

---
+++ Part 3/Point 1 - Loops with conditions/numbers.py
@@ -0,0 +1,17 @@
+num = int(input("Upper limit: "))
+num1 = 1
+
+while num1 < num:
+    print(num1)
+    num1 +=1
+
+#Solution code:
+#limit = int(input("Upper limit: "))
+#
+#number = 1
+#
+#while number < limit:
+#
+#    print(number)
+#
+#    number += 1
