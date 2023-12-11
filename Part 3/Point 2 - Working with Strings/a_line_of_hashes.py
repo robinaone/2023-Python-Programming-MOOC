af4dce38f50ff25c6613e20f2cc351e12f30ba1d
Caleb Hargraves <jmjoyh00@gmail.com>
2023-12-10 19:43:26 -0600

    Working on Part 3

---
+++ Part 3/Point 2 - Working with Strings/a_line_of_hashes.py
@@ -0,0 +1,10 @@
+width = int(input("Width: "))
+row = "#"
+
+while len(row) < width:
+    row += "#"
+print(f"{row}")
+
+#The sample solution makes me want to blow my brains out.
+#width = int(input("Width: "))
+#print("#"*width)
---
+++ Part 3/Point 2 - Working with Strings/a_rectangle_of_hashes.py
@@ -0,0 +1,8 @@
+width = int(input("Width: "))
+height = int(input("Height: "))
+
+y = 1
+
+while y <= height:
+    print("#" * width)
+    y += 1
